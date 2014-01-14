48a8005d990995dadeb21f2bb7d7e2c03e1a0e15
Davide <thekage91@gmail.com>
2014-01-14 09:59:17 +0100

    Add trebles transaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Xchain::Application.routes.draw do
 
-  get "welcome/index"
-
   get '/welcome/login'
   get '/welcome/open'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
